f6e52f8c45026c9e972d6f727bf60fddabf86e23
Chris Roberts <code@chrisroberts.org>
2015-10-22 14:02:06 -0700

    Update files for release v0.1.2

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# v0.1.2
+* Default to infrastructure template
+* Default name to org + repo name
+
+# v0.1.0
+* Initial release
--- lib/fission-stacks/version.rb
+++ lib/fission-stacks/version.rb
@@ -1,5 +1,5 @@
 module Fission
   module Stacks
-    VERSION = Gem::Version.new('0.1.1')
+    VERSION = Gem::Version.new('0.1.2')
   end
 end
